8c85492f729f9dc06cc29ecd60afbbee624cf7ea
Krish <krishgupta@Krishs-MacBook-Air.local>
2024-08-21 11:31:16 +0530

    21-Aug-24

---
+++ src/L_04_literals.java
@@ -0,0 +1,8 @@
+public class L_04_literals {
+    public static void main(String[] args){
+        byte age = 36;
+        float f1 = 54.3f;
+        double d1 = 45.44D;
+        System.out.println(d1);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello and welcome!");
+//
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println("i = " + i);
+//        }
+        // AddTwoNumber --> Pascal Naming Convention
+        // addTwoNumber --> camelCase Naming Convention
+
+         int num1 = 10;
+         int num2 = 20;
+         int num3 = 30;
+         int add = (num1 + num2  + num3);
+         System.out.println("Addition Of " + num1 + " + " + num2 + " + " + num3 + " = " + add);
+    }
+}
